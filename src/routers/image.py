8578aaa8ad09b954697464daaae9f99f8e7c994c
RuslanUsmanov <usmanovruslan322@gmail.com>
2024-04-26 01:05:00 +0500

    Начал делать роут

--- src/routers/image.py
+++ src/routers/image.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import models
+from src.database import get_db_async
 
 router = APIRouter(tags=["image"])
 
 
-@router.get("/process_image")
-def process_image():
-    return {"message": "yep, it works"}
+@router.post("/process_image/{pipeline_id}")
+async def process_image(
+    pipeline_id: int,
+    image: UploadFile = File(...),
+    db: AsyncSession = Depends(get_db_async),
+):
+    pipeline = await db.get(models.Pipeline, pipeline_id)
+    if not pipeline:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Pipeline not found"
+        )
+
+    image_bytes = await image.read()
+
+    return pipeline.tasks
