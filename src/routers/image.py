b9e4bf5b3d957f3ab1e44e31b9d629bac0d76642
RuslanUsmanov <usmanovruslan322@gmail.com>
2024-04-29 23:23:39 +0500

    Наш главный роут теперь вызывает runner

--- src/routers/image.py
+++ src/routers/image.py
@@ -1,24 +1,27 @@
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from base64 import b64encode
+
+from fastapi import APIRouter, Depends, File, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src import models
+from src import schemas
 from src.database import get_db_async
+from src.service import runner
 
 router = APIRouter(tags=["image"])
 
 
-@router.post("/process_image/{pipeline_id}")
+@router.post(
+    "/process_image/{pipeline_id}", response_model=schemas.PipelineRun
+)
 async def process_image(
     pipeline_id: int,
     image: UploadFile = File(...),
     db: AsyncSession = Depends(get_db_async),
 ):
-    pipeline = await db.get(models.Pipeline, pipeline_id)
-    if not pipeline:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Pipeline not found"
-        )
+    """Обработка изображения по указанному пайплайну"""
 
-    image_bytes = await image.read()
+    image = await image.read()
 
-    return pipeline.tasks
+    image_bytes = b64encode(image).decode()
+
+    return await runner.create_and_run_pipeline(db, pipeline_id, image_bytes)
